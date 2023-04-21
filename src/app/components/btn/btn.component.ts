c8ed5996aa934841bc75cceaf73376fd22f78ce0
Dtheran <dtheran1@gmail.com>
2023-04-21 11:08:01 -0500

    add dynamic class

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-btn',
   templateUrl: './btn.component.html'
 })
 export class BtnComponent {
-  
+
+  @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button'
+  @Input() color: 'primary' | 'success' | 'danger' = 'danger'
+
+  get colors() { // Al ser un getters se ejecuta inmediatamente
+    return {
+      'bg-green-700': this.color === 'success',
+      'hover:bg-green-800': this.color === 'success',
+      'focus:bg-green-300': this.color === 'success',
+      'bg-blue-700': this.color === 'primary',
+      'hover:bg-blue-800': this.color === 'primary',
+      'focus:bg-blue-300': this.color === 'primary',
+      'bg-red-700': this.color === 'danger',
+      'hover:bg-red-800': this.color === 'danger',
+      'focus:bg-red-300': this.color === 'danger',
+    }
+  }
+
 }
